cbdb298c499276b66e8de0271aa78ac05b33ed5a
Jacky11 <49830892+Jacky-111111@users.noreply.github.com>
2024-09-12 23:26:42 -0400

    Update stm.php Eng

--- stm.php
+++ stm.php
@@ -46,11 +46,11 @@ try {
     $mail->isHTML(true);
     $mail->Subject = '信息提交成功通知';
     $mail->Body = "<p>尊敬的用户，</p>
-<p>感谢您使用我们的小程序，并提交了您的碳减排相关证据！我们很高兴地通知您，您的信息已成功提交。</p>
-<p>您的贡献对于环境保护和碳减排事业至关重要。我们深知您对环境的关注和努力，您的行动将为创造更可持续的未来做出重要贡献。</p>
-<p>如果您有任何疑问或需要进一步的帮助，请随时联系我们的团队。我们将竭诚为您提供支持和指导。</p>
-<p>感谢您的积极参与和贡献！</p>
-<p>诚挚的问候，<br>Carbon Track</p>";
+<p>感谢您使用我们的小程序，并提交了您的碳减排相关证据！我们很高兴地通知您，您的信息已成功提交。Thank you for using our platform and submitting your evidence of carbon reduction! We are pleased to inform you that your record has been successfully submitted.</p>
+<p>您的贡献对于环境保护和碳减排事业至关重要。我们深知您对环境的关注和努力，您的行动将为创造更可持续的未来做出重要贡献。Your contribution is vital to the cause of environmental protection and carbon reduction. We know that your concern for the environment and your efforts will make an important contribution to creating a more sustainable future.</p>
+<p>如果您有任何疑问或需要进一步的帮助，请随时联系我们的团队。我们将竭诚为您提供支持和指导。If you have any questions or need further assistance, please feel free to contact our team. We will be happy to provide you with support and guidance.</p>
+<p>感谢您的积极参与和贡献！Thank you for your active participation and contribution!</p>
+<p>诚挚的问候，Sincere regards,<br>CarbonTrack</p>";
 
     // 发送邮件
     $mail->send();
